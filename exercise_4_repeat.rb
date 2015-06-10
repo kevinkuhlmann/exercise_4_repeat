c6169e35907c224569a11a932685ea9d55487866
Kevin Kuhlmann <kevinkuhlmann0526@gmail.com>
2015-06-10 01:31:13 -0400

    Completed fourth exercise

---
+++ exercise_4_repeat.rb
@@ -0,0 +1,16 @@
+one_to_100 = (1..100)
+
+one_to_100.each do |number|
+	if number % 3 == 0 && number % 5 == 0
+		puts "BitMaker"
+	elsif number % 5 == 0
+		puts "Maker"
+	elsif number % 3 == 0
+		puts "Bit"
+	else
+		puts number
+	end
+end
+
+		
+		
